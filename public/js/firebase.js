2a22de2d8d962da0149c396a8752361b5506f16b
diyorbekTursunov <diyorbektursunov570@gmail.com>
2024-08-14 17:10:23 +0500

    deguib

---
+++ public/js/firebase.js
@@ -0,0 +1,20 @@
+let firebaseConfig = {
+    apiKey: "AIzaSyCWWW7e8y33PnPXw_Svtw3XBktMFdC4xns",
+
+    authDomain: "blog-c36e1.firebaseapp.com",
+
+    projectId: "blog-c36e1",
+
+    storageBucket: "blog-c36e1.appspot.com",
+
+    messagingSenderId: "267812341097",
+
+    appId: "1:267812341097:web:4f3294c4c43baf079b69f8",
+
+    measurementId: "G-G3FFNN3KW1"
+
+};
+
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
